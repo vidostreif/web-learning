8cce4246875b101be8674402e8893cf220e9280c
VIDO-home <vido1@yandex.ru>
2023-05-10 22:23:10 +0300

    Добавление скелета

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type newStyleType = {
+  [key: string]: string
 }
 
-export default App;
+interface dataType {
+  order: number
+  description: string
+  answer: React.CSSProperties
+}
+
+function App() {
+  let [cssText, setCssText] = useState<string>('')
+  let [styleOfTask, setStyleOfTask] = useState<React.CSSProperties>({})
+  let [data, setData] = useState<dataType[]>()
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+  }, [])
+
+  // Проверяем введенные данные пользователя
+  const onChangeCssText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCssText(e.target.value)
+
+    let array = e.target.value.split(' ').join('').split(':')
+    let newStyle: newStyleType = {}
+    if (array.length === 2) {
+      if (array[1].slice(-1) === ';') {
+        array[1] = array[1].slice(0, -1)
+      }
+      newStyle[array[0]] = array[1]
+      setStyleOfTask(newStyle)
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <section className="main">
+        {data ? (
+          <>
+            <div className="task">
+              <div className="task__description">
+                <h2>Задание {data[0].order}</h2>
+                <p>{data[0].description}</p>
+              </div>
+              <div className="task__editor">
+                <h2>Редактор</h2>
+                <div>.text &#123;</div>
+                <input value={cssText} onChange={onChangeCssText} />
+                <div>&#125;</div>
+              </div>
+            </div>
+            <div className="player">
+              <h2>Визуальное отображение</h2>
+
+              <div style={styleOfTask}>тектст теста</div>
+            </div>
+          </>
+        ) : (
+          'Загрузка...'
+        )}
+      </section>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default App
