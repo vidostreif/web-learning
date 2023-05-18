d5c1cae4cfba7e7bad6e0dba6c4739b545839232
VIDO-home <vido1@yandex.ru>
2023-05-18 20:18:34 +0300

    изменение размера в поле ввода

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
 import CssTask from './pages/cssTask'
