e11afdd8bf92e090c09d7c39b7b1eee05e62b9b7
VIDO-home <vido1@yandex.ru>
2023-05-18 18:14:10 +0300

    Подключение шрифта Source Code Pro

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import './App.scss'
 import CssTask from './pages/cssTask'
 import Home from './pages/home'
 
